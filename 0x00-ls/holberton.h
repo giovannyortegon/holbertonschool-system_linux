70f5606fb445e5fa98a55b9c2f0c4a5494072d04
Skillhh <ortegon.giovanny@hotmail.com>
2020-04-13 18:35:09 -0500

    [ADD] header with common libraries

---
+++ 0x00-ls/holberton.h
@@ -0,0 +1,15 @@
+#ifndef __HOLBERTON_H__
+#define __HOLBERTON_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <pwd.h>
+
+#endif /* HOLBERTON.H */
